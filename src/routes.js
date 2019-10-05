87a495088325eb19bca40b2e24240fe25b87abe1
Antonio Ricardo <antonioricardoarfs@gmail.com>
2019-10-05 00:31:22 -0400

    Creating pattern MVC and create routes to sessions users

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const SessionController = require('./controllers/SessionController');
 const routes = express.Router();
 
 routes.post('/users', (req, res) => {
--- src/server.js
+++ src/server.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const app = express();
 
+mongoose.connect(
+	'mongodb+srv://antonioricardo:rub32912289@omnistack-x3jb8.mongodb.net/admin?retryWrites=true&w=majority',
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	}
+);
+
 app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
