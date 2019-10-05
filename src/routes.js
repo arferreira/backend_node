861728160018e81542313f0045d0b458de8ef3b1
Antonio Ricardo <antonioricardoarfs@gmail.com>
2019-10-05 01:37:54 -0400

    Creating model to scheduling spots

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const multer = require('multer');
 const uploadConfig = require('./config/upload');
 
 const SessionController = require('./controllers/SessionController');
+const DashboardController = require('./controllers/DashboardController');
 const SpotController = require('./controllers/SpotController');
 
 const routes = express.Router();
@@ -10,6 +11,10 @@ const upload = multer(uploadConfig);
 
 // Rotas
 routes.post('/sessions', SessionController.store);
+
+routes.get('/spots', SpotController.index);
 routes.post('/spots', upload.single('thumbnail'), SpotController.store);
+
+routes.get('/dashboard', DashboardController.show);
 
 module.exports = routes;
